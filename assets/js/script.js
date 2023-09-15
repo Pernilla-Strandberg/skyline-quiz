ac0a6d13ba15ae26d660f8f409f292069a4bb56d
Pernilla Strandberg <pernillastrandberg87@gmail.com>
2023-09-15 19:33:09 +0000

    Feat: Set counters, display first view with randomized quiz data options

--- assets/js/script.js
+++ assets/js/script.js
@@ -93,6 +93,7 @@ const quizData = [
     },
 ];
 
+// Get elements from DOM
 const quizContainer = document.getElementById('quiz-container');
 const quizStartContainer = document.getElementById('quiz-start');
 const newQuizButton = document.getElementById('new-quiz-button');
@@ -100,5 +101,57 @@ const quizQuestionContainer = document.getElementById('quiz-question');
 const submitButton = document.getElementById('submit-button');
 const progressBar = document.getElementById('progress-bar');
 const resultContainer = document.getElementById('quiz-result');
+
+// Set empty counters
+let progressDots = [];
+let currentQuestion = 0;
+let score = 0;
+let incorrectAnswers = [];
+
+// Display first view of container and button, hide other views and elements
+function displayQuizStart() {
+    quizQuestionContainer.style.display = 'none';
+    submitButton.style.display = 'none';
+    quizStartContainer.style.display = 'block'
+    newQuizButton.style.display = 'inline-block';
+    resultContainer.style.display = 'none'
+}
+
+// Display quiz data
+function displayQuestion() {
+    const questionData = quizData[currentQuestion];
+    const quizImage = document.getElementById('quiz-image');
+    const questionText = document.getElementById('quiz-question-text');
+    const quizForm = document.getElementById('quiz-form');
+
+    quizImage.src = questionData.image;
+    questionText.textContent = questionData.question;
+
+    // Reset choices
+    quizForm.innerHTML = '';
+
+    // Mix question data options
+    const mixedOptions = [...questionData.options];
+    mixArray(mixedOptions);
+
+    // Create radio buttons and mix the options
+    for (let i = 0; i < mixedOptions.length; i++) {
+        const label = document.createElement('label');
+        const input = document.createElement('input');
+        input.type = 'radio';
+        input.name = 'quiz';
+        input.value = mixedOptions[i];
+        label.appendChild(input);
+        label.appendChild(document.createTextNode(mixedOptions[i]));
+        quizForm.appendChild(label);
+    }
+
+    // Show quiz form and progress bar
+    quizForm.style.display = 'block';
+    progressBar.style.display = 'block';
+    updateProgress();
+
+    
+}
 
 
