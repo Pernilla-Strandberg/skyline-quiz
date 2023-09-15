3efe41c6c54f2674477abc99f82d686efd899fe4
Pernilla Strandberg <pernillastrandberg87@gmail.com>
2023-09-15 11:54:21 +0000

    Feat: Add quizData in array to hold questions

---
+++ assets/js/script.js
@@ -0,0 +1,94 @@
+// Store quizData in array
+
+const quizData = [
+    {
+      image: 'assets/images/image1.jpg',
+      question: 'Skyline?',
+      options: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4', 'Choice 5'],
+      answer: 'Choice 2',
+    },
+    {
+        image: 'image2.jpg',
+        question: 'Question 2?',
+        options: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4', 'Choice 5'],
+        answer: 'Choice 2',
+    },
+    {
+        image: 'image3.jpg',
+        question: 'Question 3?',
+        options: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4', 'Choice 5'],
+        answer: 'Choice 2',
+    },
+    {
+        image: 'image4.jpg',
+        question: 'Question 4?',
+        options: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4', 'Choice 5'],
+        answer: 'Choice 2',
+    },
+    {
+        image: 'image5.jpg',
+        question: 'Question 5?',
+        options: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4', 'Choice 5'],
+        answer: 'Choice 2',
+    },
+    {
+        image: 'image6.jpg',
+        question: 'Question 6?',
+        options: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4', 'Choice 5'],
+        answer: 'Choice 2',
+    },
+    {
+        image: 'image7.jpg',
+        question: 'Question 7?',
+        options: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4', 'Choice 5'],
+        answer: 'Choice 2',
+    },
+    {
+        image: 'image8.jpg',
+        question: 'Question 8?',
+        options: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4', 'Choice 5'],
+        answer: 'Choice 2',
+    },
+    {
+        image: 'image9.jpg',
+        question: 'Question 9?',
+        options: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4', 'Choice 5'],
+        answer: 'Choice 2',
+    },
+    {
+        image: 'image10.jpg',
+        question: 'Question 10?',
+        options: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4', 'Choice 5'],
+        answer: 'Choice 2',
+    },
+    {
+        image: 'image11.jpg',
+        question: 'Question 11?',
+        options: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4', 'Choice 5'],
+        answer: 'Choice 2',
+    },
+    {
+        image: 'image12.jpg',
+        question: 'Question 12?',
+        options: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4', 'Choice 5'],
+        answer: 'Choice 2',
+    },
+    {
+        image: 'image13.jpg',
+        question: 'Question 13?',
+        options: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4', 'Choice 5'],
+        answer: 'Choice 2',
+    },
+    {
+        image: 'image14.jpg',
+        question: 'Question 14?',
+        options: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4', 'Choice 5'],
+        answer: 'Choice 2',
+    },
+    {
+        image: 'image15.jpg',
+        question: 'Question 15?',
+        options: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4', 'Choice 5'],
+        answer: 'Choice 2',
+    },
+];
